f7071f3a2da86e5158191f204c3d4dddee8392f3
HowdenG <graham_howden1@yahoo.co.uk>
2014-09-04 11:28:10 +0100

    make immutable filedata

--- src/main/java/com/exsoloscript/skriptlib/data/FileData.java
+++ src/main/java/com/exsoloscript/skriptlib/data/FileData.java
@@ -1,49 +1,30 @@
 package com.exsoloscript.skriptlib.data;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
-
 public class FileData {
 
-	private String name;
-	private Map<String, String> properties;
+	private final String name;
+	private final String version;
+	private final String author;
 
-	public FileData(String name) {
+	public FileData(String name, String version, String author)
+    {
 		this.name = name;
-		this.properties = new HashMap<String, String>();
+        this.version = version;
+        this.author = author;
 	}
 
-	public String getName() {
+	public String getName()
+    {
 		return name;
 	}
 
-	public Map<String, String> getProperties() {
-		return properties;
-	}
+    public String getVersion()
+    {
+        return version;
+    }
 
-	public static List<FileData> parse(String[] entries) {
-		List<FileData> data = new ArrayList<FileData>();
-
-		for (String s : entries) {
-			s = s.trim();
-			String[] infos = s.split(" ");
-			if (infos.length > 0) {
-				FileData fd = new FileData(infos[0]);
-				infos = Arrays.copyOfRange(infos, 1, infos.length);
-				for (String p : infos) {
-					String[] prop = p.split("=");
-					if (prop.length == 2) {
-						fd.getProperties().put(prop[0], prop[1]);
-					}
-				}
-				
-				data.add(fd);
-			}
-		}
-
-		return data;
-	}
+    public String getAuthor()
+    {
+        return author;
+    }
 }
